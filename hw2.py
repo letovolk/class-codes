d247dad99fa234e0ca1dea9d735ae007656e8836
letovolk <cenker.yazkurt@std.bogazici.edu.tr>
2024-11-12 12:26:23 +0300

    Update hw2.py
    # Recursive First Draft

--- hw2.py
+++ hw2.py
@@ -1,6 +1,7 @@
 import time
 import random
 import itertools
+
 random.seed(2024402252)
 # Cost Matrix Generator 
 def cost_matrix_generator(n):
@@ -65,3 +66,71 @@ for ith_cost_matrix in cost_matrix_list:
 
 print(f"Total time for all matrices: {total_time: } seconds")
 print(" ")
+
+
+
+print("Recursive Approach Starts")
+print(" ")
+
+def total_cost_calculator_recursive_approach(cost_matrix_r, visited, not_visited, cache={}):
+    
+    if not any(not_visited):  # All cases are visited
+        return 0
+
+    current_task = tuple(visited) # Immutability to work with dictinories 
+    if current_task in cache:
+        return cache[current_task]
+    
+    # Base to start memoization 
+
+    min_cost = float('inf')
+
+    for cell in range(len(cost_matrix_r)):
+        if not_visited[cell]: 
+            if any(visited): 
+                last_visited = visited.index(True, 0) 
+                transition_cost = cost_matrix_r[last_visited][cell]
+            else:
+                transition_cost = 0  
+
+            # Backtrack 
+            new_visited = visited[:]
+            new_not_visited = not_visited[:]
+            new_visited[cell] = True
+            new_not_visited[cell] = False
+
+            # Recursive call to calculate the cost for remaining nodes
+            total_cost = transition_cost + total_cost_calculator_recursive_approach(
+                cost_matrix_r, new_visited, new_not_visited, cache
+            )
+
+            # Update minimum cost
+            min_cost = min(min_cost, total_cost)
+
+    # Store in cache
+    cache[current_task] = min_cost
+    return min_cost
+
+total_time = 0
+matrix_size = 6
+
+
+for jth_cost_matrix in cost_matrix_list:
+    start_time = time.time()
+
+    # Initial conditions 
+    initial_visited = [False] * len(jth_cost_matrix)
+    initial_not_visited = [True] * len(jth_cost_matrix)
+
+    min_cost = total_cost_calculator_recursive_approach(jth_cost_matrix, initial_visited, initial_not_visited)
+
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    total_time += iteration_time
+
+    print(f"Minimum cost for {matrix_size}x{matrix_size} matrix: {min_cost}")
+    print(f"Time elapsed for the {matrix_size}x{matrix_size} matrix: {iteration_time: } seconds\n")
+    matrix_size += 1
+
+print(f"Total time for all matrices: {total_time: } seconds")
+
